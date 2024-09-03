a681cd5072a5d53bc402d05d373b2988ea674938
peramacpf <pedro14266@gmail.com>
2024-09-03 16:59:29 -0300

    estilizando o site

--- scripts.js
+++ scripts.js
@@ -26,58 +26,73 @@ formulario.addEventListener('submit', function(event) {
     event.preventDefault();
 
     let pokemon = document.getElementById("nome_pokemon").value.toLowerCase();
-    const apiURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+    if (pokemon == "") {
+        alert("Nenhum Pokémon foi digitado!")
+    } else {
+        const apiURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
 
-    fetch(apiURL)
-        .then(response => response.json())
-        .then(dados => {
-            
-            const listaTipos = document.getElementById("tipos");
-            listaTipos.innerHTML = "";
-            const listaHabilidades = document.getElementById("habilidades");
-            listaHabilidades.innerHTML = "";
-
-            if (dados) {
-
-                document.getElementById("pokemon").innerText = dados.name;
-
-                dados.types.forEach(tipoPokemon => {
-                    let li = document.createElement("li");
-                    li.textContent = tipoPokemon.type.name;
-                    li.style.cssText = `background-color: ${coresPokemon[tipoPokemon.type.name]};
-                                        color: #fff; border: 1px solid #000;
-                                        text-align: center; width: 50px;
-                                        height: 20px; padding: 5px; 
-                                        border-radius: 10px; display: 
-                                        inline-block; 
-                                        margin-right: 5px;`;
-                    listaTipos.appendChild(li);
-                });
-                dados.abilities.forEach(habilidades => {
-                    let li = document.createElement("li");
+        fetch(apiURL)
+            .then(response => response.json())
+            .then(dados => {
+    
+                const listaTipos = document.getElementById("tipos");
+                listaTipos.innerHTML = "";
+    
+                const listaHabilidades = document.getElementById("habilidades");
+                listaHabilidades.innerHTML = "";
+    
+                if (dados) {
+    
+                    let nomePokemon = dados.name;
+                    let nomePokemonFormatado = nomePokemon[0].toUpperCase() + nomePokemon.substr(1)
+                    document.getElementById("pokemon").innerText = nomePokemonFormatado;
                     
-                    fetch(habilidades.ability.url)
-                    .then(response => response.json())
-                    .then(dadosHabilidades => {
-                        const descricaoHabilidade = dadosHabilidades.effect_entries.find(idioma => idioma.language.name === 'en'
-                        );
-
-                        li.textContent = dadosHabilidades.name + ": " + descricaoHabilidade.effect;
+                    dados.types.forEach(tipoPokemon => {
+                        let li = document.createElement("li");
+                        li.textContent = tipoPokemon.type.name;
+                        li.style.cssText = `background-color: ${coresPokemon[tipoPokemon.type.name]};
+                                            color: #fff; 
+                                            border: 1px solid #000;
+                                            text-align: center; 
+                                            width: 50px;
+                                            height: 20px; 
+                                            padding: 5px; 
+                                            border-radius: 10px; 
+                                            display: inline-block; 
+                                            margin-right: 35px;`;
+                        listaTipos.appendChild(li);
                     });
-                listaHabilidades.appendChild(li);
+                    document.getElement
+                    dados.abilities.forEach(habilidades => {
+                        let li = document.createElement("li");
+                        
+                        fetch(habilidades.ability.url)
+                        .then(response => response.json())
+                        .then(dadosHabilidades => {
+                            const descricaoHabilidade = dadosHabilidades.effect_entries.find(idioma => idioma.language.name === 'en'
+                            );
+    
+                            let nomeHabilidade = dadosHabilidades.name;
+                            let nomeHabilidadeFormatado = nomeHabilidade[0].toUpperCase() + nomeHabilidade.substr(1)
+                            li.innerHTML = `<strong>${nomeHabilidadeFormatado}</strong>: ${descricaoHabilidade.effect}`;
+                        });
+                    listaHabilidades.appendChild(li);
+    
+                    const imagem = document.getElementById("imagemPoke");
+    
+                    imagem.src = dados.sprites.front_default;
+                    imagem.style.display = "block";
 
-                const imagem = document.getElementById("imagemPoke");
-
-                imagem.src = dados.sprites.front_default;
-                imagem.style.display = "block";
+                    let tituloHabilidades = document.getElementById("tipos_habilidades");
+                    tituloHabilidades.style.display = "block";
+                });
+                } else {
+                    console.log("Pokémon não encontrado!");
+                }
+                console.log(dados);
+            })
+            .catch(error => {
+                console.error("Erro na requisição:", error);
             });
-            } else {
-                console.log("Pokémon não encontrado!");
-            }
-            console.log(dados);
-        })
-        .catch(error => {
-            console.error("Erro na requisição:", error);
-        });
-});
-
+        }
+    });
